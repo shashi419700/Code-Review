d4aaba7982b4ad5c1242bf62abecc2915998521f
Shashi419700 <codewithshashi009@gmail.com>
2025-09-13 00:05:27 +0530

    Added package.json and lock file

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,6 +3,5 @@ const app = require('./src/app')
 
 
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
